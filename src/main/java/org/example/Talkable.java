4f1e01ec980fdc24dad03fd00dccdc8df0dd1c7e
danivescia <danivescia@gmail.com>
2025-11-06 09:26:18 -0300

    Add comentários Talkable

--- src/main/java/org/example/Talkable.java
+++ src/main/java/org/example/Talkable.java
@@ -1,14 +1,13 @@
 package org.example;
 
 /**
- * Interface que define o comportamento de qualquer entidade que fale
- * <p>
+ * <p>Interface que define o comportamento de qualquer entidade que fale </p>
+ * 
  */
 
 
  /**
   * @return uma string do som correspondente a entidade que implementa a função
-  * <p>
   */
 public interface Talkable {
     String talk();
